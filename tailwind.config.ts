78e4a267673542a27e968e6b6b513b757a83c74f
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-05-15 16:34:56 +0100

    style: Add palette colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Color Hunt Palette
+        palette: {
+          1: "#F3EEEA",
+          2: "#EBE3D5",
+          3: "#B0A695",
+          4: "#776B5D",
+        },
+      },
+    },
   },
   plugins: [],
 };
