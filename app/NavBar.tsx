ec8ee9d0553727adbac856e6f3cdad27d534c734
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-05-16 23:40:27 +0100

    chore: Update border color for the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,7 +2,7 @@ import NavLinks from "./NavLinks";
 
 const NavBar: React.FC = () => {
   return (
-    <header className="border-b border-palette-4 bg-palette-2">
+    <header className="border-b border-palette-3 bg-palette-2">
       <div className="mx-6 max-w-7xl 2xl:mx-auto">
         <div className="flex h-16 items-center">
           {/* Logo */}
