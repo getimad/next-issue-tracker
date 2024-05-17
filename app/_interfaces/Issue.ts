325fa72c24491e1e650b89c81f0129212a58b97f
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-05-17 18:47:12 +0100

    refactor: Update Status enum in Issue interface with the Enums from prisma

--- app/_interfaces/Issue.ts
+++ app/_interfaces/Issue.ts
@@ -1,16 +1,12 @@
+import { $Enums } from "@prisma/client";
+
 interface Issue {
   id: number;
   title: string;
   description: string;
-  status: Status;
+  status: $Enums.Status;
   createdAt: Date;
   updatedAt: Date;
-}
-
-enum Status {
-  OPEN,
-  IN_PROGRESS,
-  CLOSED,
 }
 
 export default Issue;
