c603e3541a3c0b60289a7401ad770d0da4226528
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-05-17 21:15:52 +0100

    feat: Add DELETE request for issue

--- app/api/issues/[issueId]/route.ts
+++ app/api/issues/[issueId]/route.ts
@@ -27,5 +27,24 @@ export async function PATCH(
     data: validation.data,
   });
 
-  return Response.json(updatedIssue);
+  return Response.json(updatedIssue, { status: 200 });
+}
+
+export async function DELETE(
+  request: Request,
+  { params: { issueId } }: { params: { issueId: string } },
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(issueId) },
+  });
+
+  if (!issue) {
+    return Response.json({ error: "Issue not found" }, { status: 404 });
+  }
+
+  const deletedIssue = await prisma.issue.delete({
+    where: { id: parseInt(issueId) },
+  });
+
+  return Response.json(deletedIssue, { status: 200 });
 }
